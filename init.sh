be4b12fd4d17738c865bf04842584bf8c75fe905
B1ueber2y <b1ueber2y@gmail.com>
2019-08-10 18:18:37 -0400

    update init script.

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ mkdir -p ./data
 
 cp -r src/reppoints_head/* mmdetection/mmdet/models/anchor_heads/
 cp -r src/reppoints_detector/* mmdetection/mmdet/models/detectors/
+cp -r src/reppoints_generator/* mmdetection/mmdet/core/anchor/
+cp -r src/reppoints_assigner/* mmdetection/mmdet/core/bbox/assigners/
 
 cd mmdetection
 pip install -v -e .
