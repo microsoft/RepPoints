652a05c31edda04d0b64f0e2452fc9d204ba7e95
Ze Yang <yangze0930@outlook.com>
2019-08-10 19:05:01 +0800

    Add reppoints head and reppoints detector

---
+++ src/reppoints_head/__init__.py
@@ -0,0 +1,15 @@
+from .anchor_head import AnchorHead
+from .fcos_head import FCOSHead
+from .ga_retina_head import GARetinaHead
+from .ga_rpn_head import GARPNHead
+from .guided_anchor_head import FeatureAdaption, GuidedAnchorHead
+from .retina_head import RetinaHead
+from .rpn_head import RPNHead
+from .ssd_head import SSDHead
+from .reppoints_head import RepPointsHead
+
+__all__ = [
+    'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead',
+    'GARPNHead', 'RetinaHead', 'GARetinaHead', 'SSDHead', 'FCOSHead',
+    'RepPointsHead'
+]
