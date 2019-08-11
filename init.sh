44316a572540a62b295f5961df228c12fc31f834
B1ueber2y <b1ueber2y@gmail.com>
2019-08-10 20:13:49 -0400

    add script for downloading coco 2017 dataset.

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-mkdir -p ./data
+# download data
+sh ./download_coco2017.sh
 
+# copy files
 cp -r src/reppoints_head/* mmdetection/mmdet/models/anchor_heads/
 cp -r src/reppoints_detector/* mmdetection/mmdet/models/detectors/
 cp -r src/reppoints_generator/* mmdetection/mmdet/core/anchor/
 cp -r src/reppoints_assigner/* mmdetection/mmdet/core/bbox/assigners/
 
+# install mmdetection
 cd mmdetection
 pip install -v -e .
 cd ..
